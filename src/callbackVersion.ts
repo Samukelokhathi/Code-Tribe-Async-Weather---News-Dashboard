2f555a630cd4af75f9cf933fbd894c075ffb8a04
Samkelo Khathi <sbkhathi005@gmail.com>
2026-09-14 11:43:55 +0200

    Added call back functionality to fetch and display weather and news json

--- src/callbackVersion.ts
+++ src/callbackVersion.ts
@@ -1,1 +1,20 @@
- 
+const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=-29.86&longitude=31.02&current_weather=`;
+const newsUrl = `https://dummyjson.com/posts/1`;
+
+function fetchData(url: string, callback: (data: any) => void): void {
+  console.log(`Fetching data..`);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      callback(data);
+    })
+    .catch((error) => console.error("Error:", error));
+}
+
+fetchData(weatherUrl, (weatherData) => {
+  console.log("Displaying weather response.. \n", weatherData);
+
+  fetchData(newsUrl, (newsData) => {
+    console.log("\n Displaying news response.. \n", newsData);
+  });
+});
